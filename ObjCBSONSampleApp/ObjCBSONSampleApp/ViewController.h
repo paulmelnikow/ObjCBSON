df82c3ec394f660483ba0e68749d76948e4021ad
Wojciech A. Koszek <wojciech@koszek.com>
2016-04-16 18:43:31 -0700

    Add sample ObjCBSON project.

---
+++ ObjCBSONSampleApp/ObjCBSONSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCBSONSampleApp
+//
+//  Created by Wojciech Adam Koszek (h) on 16/04/2016.
+//  Copyright © 2016 Wojciech Adam Koszek (h). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjCBSONSampleApp/ObjCBSONSampleApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ObjCBSONSampleApp
+//
+//  Created by Wojciech Adam Koszek (h) on 16/04/2016.
+//  Copyright © 2016 Wojciech Adam Koszek (h). All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ObjCBSON/BSONSerializer.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
